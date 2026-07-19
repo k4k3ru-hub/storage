141023909f3020ab7b9cbde19bad8d308b77d0bb
Kakeru Sato <sato.kakeru3@gmail.com>
2026-07-19 12:29:33 +0900

    under dev

---
+++ go/mysql/kms/payload/smtp.go
@@ -0,0 +1,59 @@
+//
+// smtp.go
+//
+package payload
+
+import (
+    "encoding/json"
+    "fmt"
+)
+
+
+//
+// SMTP payload.
+//
+type SMTP struct {
+    Host     string `json:"host,omitempty"`
+    Port     int    `json:"port,omitempty"`
+    Username string `json:"username,omitempty"`
+    Password string `json:"password,omitempty"`
+}
+
+
+//
+// Build plaintext.
+//
+// Version:
+//   - 2026-07-19: Added.
+//
+func (s SMTP) BuildPlaintext() ([]byte, error) {
+    plaintext, err := json.Marshal(s)
+    if err != nil {
+        return nil, fmt.Errorf("failed to build  payload plaintext: %w", err)
+    }
+
+    return plaintext, nil
+}
+
+
+//
+// Parse SMTP payload.
+//
+// Version:
+//   - 2026-07-19: Added.
+//
+func ParseSMTP(plaintext []byte) (*SMTP, error) {
+    if len(plaintext) == 0 {
+        return nil, fmt.Errorf("failed to parse SMTP payload: plaintext=empty")
+    }
+
+    var payload SMTP
+    if err := json.Unmarshal(plaintext, &payload); err != nil {
+        return nil, fmt.Errorf(
+            "failed to parse SMTP payload: %w",
+            err,
+        )
+    }
+
+    return &payload, nil
+}
